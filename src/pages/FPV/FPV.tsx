98febb39fdac146cc37c7a6bce378f563cb81dac
Altrul <AlexanderOnko@gmail.com>
2024-09-08 22:23:52 +0300

    Added grass. Does not render

--- src/pages/FPV/FPV.tsx
+++ src/pages/FPV/FPV.tsx
@@ -1,8 +1,10 @@
-import { Cloud, PointerLockControls, Sky } from "@react-three/drei";
+import * as THREE from "three";
+import { Clouds, Cloud, PointerLockControls, Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { TextureLoader } from "three";
 import Building from "./objects/Building/Building";
+import Grass from "./objects/Grass/Grass";
 
 function Ground() {
   return (
@@ -20,9 +22,12 @@ export default function FPV() {
         <PointerLockControls />
         <Sky sunPosition={[20, 20, 20]} />
 
-        <Cloud position={[0,50,0]} segments={50} bounds={[100, 10, 100]} volume={30} speed={0.01} color="white" opacity={0.1} />
-        <ambientLight intensity={0.5} />
+        <Clouds material={THREE.MeshLambertMaterial}>
+          <Cloud position={[0,500,0]} segments={100} bounds={[500, 10, 500]} volume={100} speed={0.01} color="white" opacity={0.1} concentrate={"random"} />
+        </Clouds>
+        <ambientLight intensity={4.0} />
         <Ground />
+        <Grass />
         <Building position={[0, 0]} />
         <Building position={[5, 5]} />
       </Canvas>

db0b605e144fe1c3ec875e06e891d4441ee09ba6
Artur Lukianov <noemail@saved.me>
2024-09-08 18:24:31 +0300

    icons

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,34 +1,51 @@
 import React from 'react';
+import ReactElement from 'react';
 import { Link, Outlet } from "react-router-dom";
 import './style.css';
-import { css } from '@emotion/react'
+import { css } from '@emotion/css'
 import styled from '@emotion/styled';
+import { Map, NotebookText, Waypoints } from 'lucide-react';
 
 
-function NavbarLink(props: { to: string, text: string }) {
-    const { to, text } = props;
+function NavbarLink(props: { to: string, text?: string, icon: ReactElement }) {
+    const { to, text, icon } = props;
 
     return (
-        <Link to={to}>{text}</Link>
+        <Link className={css`
+            text-decoration: none;
+            color: var(--secondary);
+            padding: 5px;
+            border-radius: 2px;
+            font-size: 20px;
+            `} to={to}>
+            {icon}
+        </Link>
     )
 }
 
 function Navbar() {
     return (
-        <div css={css`
+        <div className={css`
+            position: absolute;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+        `}>
+            <div className={css`
             display: flex;
             flex-direction: row;
-            gap: 2px;
+            gap: 10px;
         `}>
-            <NavbarLink to='' text='Diary' />
-            <NavbarLink to='map/graph' text='Graph' />
-            <NavbarLink to='map/street' text='Street' />
+                <NavbarLink to='' text='Diary' icon={<NotebookText/>} />
+                <NavbarLink to='map/graph' text='Graph' icon={<Waypoints/>} />
+                <NavbarLink to='map/street' text='Street' icon={<Map/>}/>
+            </div>
         </div>
     )
 }
 
 const Logo = styled.h1`
-font-size: 20px;
+font-size: 30px;
 font-weight: regular;
 `
 
@@ -37,11 +54,15 @@ display: flex;
 flex-direction: row;
 align-items: center;
 gap: 1%;
+width: 100%;
 `
 
 export default function Layout() {
     return (
-        <div>
+        <div className={css`
+         margin: 10px;
+         width: 100%;
+        `}>
             <Header>
                 <Logo>DreamCatcher</Logo>
                 <Navbar />
